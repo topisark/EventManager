cdf3dd7eefe273a08ef3f37f400493de4606d9ff
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-03-13 22:55:31 +0200

    Käyttäjän kirjautumista aloitettu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Eventmanager::Application.routes.draw do
   resources :users
   resources :events
-
+  resources :sessions, only: [:new, :create]
+  delete 'signout', to: 'sessions#destroy'
   root 'events#index'
 end
